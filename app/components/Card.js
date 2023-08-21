4df4fe4998e01e27b2ac28d5af123172ea45f04c
Mohit Jeswani <mohitjeswani1210@gmail.com>
2023-08-21 19:42:04 +0530

    21 Aug, 2023 - Navbar, Card and Hero Section with search button ready to view

--- app/components/Card.js
+++ app/components/Card.js
@@ -2,12 +2,16 @@ import { PiDotsThreeCircleFill } from "react-icons/pi";
 import { AiOutlineStar, AiFillStar, AiOutlinePlusCircle } from "react-icons/ai";
 import { BsFillBookmarkCheckFill, BsBookmarkPlus } from "react-icons/bs";
 import { useState, useRef, useEffect } from "react";
+import "./components.css";
 
 const Card = ({ title, date, id, genre, score, imgUrl }) => {
   const [openToggle, setOpenToggle] = useState(false);
   const [bookMarked, setBookMarked] = useState(false);
   const [favorite, setFavorite] = useState(false);
   const divRef = useRef(null);
+  const blur = {
+    filter: "blur(8px)",
+  };
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -22,43 +26,45 @@ const Card = ({ title, date, id, genre, score, imgUrl }) => {
   }, []);
 
   return (
-    <div className=" w-40 rounded-md  overflow-hidden ">
+    <div className=" w-40 rounded-md relative overflow-hidden ">
       {/* Top Part */}
-      <div className="relative overflow-hidden w-40 max-h-53 rounded-md cursor-pointer">
+      <div className=" relative overflow-hidden w-40 max-h-53 rounded-md cursor-pointer">
         {/* Poster Image */}
         <img src={imgUrl} alt="image" className="object-cover h-54 w-40" />
 
         {/* Score */}
-        <div
-          style={{ fontFamily: "var(--font-bebas)" }}
-          className={"absolute bottom-2 right-2 flex rounded-full bg-gray-200 w-9 h-9 p-1 pt-1.5 font-bold text-sm text-center justify-center items-center"}
-        >
+        <div className="score absolute bottom-2 right-2 flex rounded-full bg-gray-200 w-9 h-9 p-1 pt-1.5 font-bold text-sm text-center justify-center items-center">
           82<sup>%</sup>
         </div>
-
-        {/* Menu Options */}
-        {!openToggle ? (
-          <button className="absolute top-2 right-2" onClick={() => setOpenToggle(true)}>
-            <PiDotsThreeCircleFill className="fill-gray-200 text-2xl opacity-70 hover:fill-cyan-500 focus:fill-cyan-500" />
-          </button>
-        ) : (
-          <div ref={divRef} className="absolute font-semibold top-8 right-2 bg-gray-200 rounded-sm flex flex-col gap-3 p-2">
-            {/* Favorite */}
+      </div>
+      {/* Menu Options */}
+      {!openToggle ? (
+        <button className="absolute top-2 right-2" style={openToggle ? blur : undefined} onClick={() => setOpenToggle(true)}>
+          <PiDotsThreeCircleFill className="fill-gray-200 text-2xl opacity-70 hover:fill-cyan-500 focus:fill-cyan-500" />
+        </button>
+      ) : (
+        <div ref={divRef} className="absolute font-semibold top-8 right-2 bg-gray-200 rounded-sm flex flex-col gap-1 p-2">
+          {/* Favorite */}
+          <div>
             <button className="flex gap-1" onClick={() => setFavorite(!favorite)}>
               {favorite ? <AiFillStar /> : <AiOutlineStar />} <p className="text-xs">Favorite</p>
             </button>
-            {/* Add to list */}
-            <button className="flex gap-1">
-              <AiOutlinePlusCircle /> <p className="text-xs">Add to list</p>
-            </button>
-            {/* Watchlist */}
-            <button className="flex gap-1" onClick={() => setBookMarked(!bookMarked)}>
-              {bookMarked ? <BsFillBookmarkCheckFill /> : <BsBookmarkPlus />}
-              <p className="text-xs">Watchlist</p>
-            </button>
+            <hr className="border border-gray-300 my-2" />
           </div>
-        )}
-      </div>
+          {/* Add to list */}
+          <div>
+            <button className="flex gap-1">
+              <AiOutlinePlusCircle /> <p className="text-xs ">Add to list</p>
+            </button>
+            <hr className="border border-gray-300 my-2" />
+          </div>
+          {/* Watchlist */}
+          <button className="flex gap-1" onClick={() => setBookMarked(!bookMarked)}>
+            {bookMarked ? <BsFillBookmarkCheckFill /> : <BsBookmarkPlus />}
+            <p className="text-xs">Watchlist</p>
+          </button>
+        </div>
+      )}
 
       {/* Title & Date */}
       <div className="px-2 py-2" style={{ lineHeight: 1.3, fontSize: "0.95rem" }}>
--- app/container/Hero.js
+++ app/container/Hero.js
@@ -1,12 +1,12 @@
-import { BiSearch } from "react-icons/bi";
+import { BiSearchAlt2 } from "react-icons/bi";
 import "./container.css";
 
 const Hero = () => {
   return (
     <div className="w-4/5 mx-auto h-72 relative overflow-hidden">
       {/* Image & Gradient Background */}
-      <img src="https://www.themoviedb.org/t/p/original/kxDCq0RbHwVzVHUkxDi67Xilrrx.jpg" className="absolute top-0 z-1 w-full object-cover image" style={{}} />
-      <div className="absolute top-0 z-0 w-full h-72 overflow-hidden opacity-80 gradient" style={{}}></div>
+      <img src="https://www.themoviedb.org/t/p/original/kxDCq0RbHwVzVHUkxDi67Xilrrx.jpg" className="absolute top-0 z-1 w-full object-cover image" />
+      <div className="absolute top-0 z-0 w-full h-72 overflow-hidden opacity-80 gradient"></div>
 
       {/* Text */}
       <div className="px-10 py-14 text-gray-200 text-left">
@@ -17,15 +17,15 @@ const Hero = () => {
       </div>
 
       {/* Search Bar */}
-      <div className="relative">
+      <div className="relative w-11/12 mx-auto">
         <input
           type="text"
           placeholder="Type the movie in your mind here..."
           id="default-input"
-          className="z-10 mx-auto bg-gray-200 font-medium text-gray-900 text-sm rounded-full focus:ring-4 focus:ring-cyan-600 focus:border-cyan-800 block w-11/12 p-3"
+          className="z-10 mx-auto bg-gray-200 font-medium text-gray-900 text-sm rounded-full focus:ring-4 focus:ring-cyan-600 focus:border-cyan-800 block w-full p-3"
         />
-        <button className="absolute flex  right-0 top-0">
-          Search <BiSearch />
+        <button className="search absolute flex top-0 py-2.5 px-9 text-md font-medium text-gray-100 rounded-full justify-end items-center gap-2">
+          Search <BiSearchAlt2 className="text-2xl" />
         </button>
       </div>
     </div>
